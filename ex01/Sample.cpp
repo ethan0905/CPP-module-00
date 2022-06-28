ced85a5e8681405f724f024725fff2e62919a2bc
Safar Ethan <ethansafar@gmail.com>
2022-06-28 15:08:50 +0200

    learning classes with namespaces and  instance

---
+++ ex01/Sample.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Sample.hpp"
+
+
+Sample::Sample( void ){
+
+	std::cout << "Constructor called" << std::endl;
+	return;
+}
+
+Sample::~Sample( void ){
+
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
