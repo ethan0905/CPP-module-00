b0c63c7162930aba2c8d52129e054b26cab39883
Safar Ethan <ethansafar@gmail.com>
2022-06-28 15:21:28 +0200

    constructor/destructor ... this->

--- ex01/Sample.cpp
+++ ex01/Sample.cpp
@@ -5,11 +5,23 @@
 Sample::Sample( void ){
 
 	std::cout << "Constructor called" << std::endl;
+
+	this->foo = 42;
+	std::cout << "Foo -> " << this->foo << std::endl;
+
+	this->bar();
+
 	return;
 }
 
 Sample::~Sample( void ){
 
 	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+void	Sample::bar( void ){
+
+	std::cout << "Member function called" << std::endl;
 	return;
 }
